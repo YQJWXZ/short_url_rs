d791370018a4312f93510780b6bf82254000f535
xiaoyi0814 <1954742838@qq.com>
2025-06-15 15:57:31 +0800

    fix: fixed commit checking and the error of proto build.

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() {
-    // 编译 protobuf 文件到 src/pb 目录
-    prost_build::Config::new()
+    let mut config = prost_build::Config::new();
+    config.bytes(["."]);
+    config
         .out_dir("src/pb")
         .compile_protos(&["abi.proto"], &["."])
         .unwrap();
 
-    // 告诉 cargo 当 proto 文件改变时重新构建
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=abi.proto");
 }
